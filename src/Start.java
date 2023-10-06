8496c220544a20f5d50e7a9d278f51e4f180edca
lukas <lukas.schuler@student.hltdornbirn.at>
2023-10-06 09:16:55 +0200

    Start des Projekts

---
+++ src/Start.java
@@ -0,0 +1,39 @@
+public class Start {
+    public static void main(String[] args) {
+
+        System.out.print("Lukas");
+        System.out.println();
+        System.out.print("Lukas");
+
+        int iZahl = 2;
+        //-2,147,483,648 to 2,147,483,647
+        char cBuchstabe;
+        float fZahl;
+        //6 to 7 decimal digits
+        double dZahl;
+        //15 decimal
+        String sSatz;
+        boolean bFalsch;
+        int[] iArray;
+        long lZahl;
+        //-9,223,372,036,854,775,808
+        byte bklein;
+        //-128 to 127
+        short sZahl;
+        //-32,768 to 32,767
+
+        iZahl = 5;
+        cBuchstabe = 'f';
+        fZahl = 34;
+        dZahl = 23884;
+        sSatz = "Satz";
+        bFalsch = true;
+        iArray = new int[4];
+        iArray[0] = 3;
+        lZahl = 345677865;
+        bklein = 23;
+        sZahl = 34;
+
+        System.out.print(dZahl);
+    }
+}
