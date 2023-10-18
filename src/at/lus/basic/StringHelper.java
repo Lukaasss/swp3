d9928c7ace7c02aa9bbcb542f0a10045e4b7b360
lukas <lukas.schuler@student.htldornbirn.at>
2023-10-18 09:08:43 +0200

    Pa

--- src/at/lus/basic/StringHelper.java
+++ src/at/lus/basic/StringHelper.java
@@ -7,36 +7,42 @@ public class StringHelper {
         Scanner scanner = new Scanner(System.in);
 
 
-        String wort ="";
+        String Palindrome = "";
 
-        System.out.println("Geben sie einen Satz ein: ");
+        System.out.println("Geben sie ein Wort ein: ");
 
-        wort = scanner.nextLine();
+        Palindrome = scanner.nextLine();
+
+        boolean result3 = isPalindrome(Palindrome);
+
+        System.out.println(result3);
+
+            String wort = "";
+
+            System.out.println("Geben sie einen Satz ein: ");
+
+            wort = scanner.nextLine();
 
 
-        char buchstabe =' ';
+            char buchstabe = ' ';
 
-        System.out.println("Geben sie einen Buchstaben ein: ");
+            System.out.println("Geben sie einen Buchstaben ein: ");
 
-        buchstabe = scanner.next().charAt(buchstabe);
+            buchstabe = scanner.next().charAt(0);
 
 
-        int result = countLetters(wort, buchstabe);
-        System.out.println("Es wurden" + result + "gefunden");
+            int result = countLetters(wort, buchstabe);
+            System.out.println("Es wurden " + result + " gefunden");
+
+
+
     }
-
-
-    public static boolean isPalindrome (String text){
-
-        return false;
-    }
-
     public static int countLetters (String text, char search){
 
         char[] textArray = text.toCharArray();
         int count = 0;
 
-        for (int i = 0; i < textArray.length; i++) {
+        for(int i = 0; i < textArray.length; i++) {
 
             if(textArray[i] == search) {
 
@@ -48,5 +54,38 @@ public class StringHelper {
 
         return count;
     }
+
+
+    public static boolean isPalindrome (String Palindrome){
+
+        int iHilfe = 0;
+        int i2 = 0;
+        boolean Ausgabe = true;
+        char[] PalindromArray = Palindrome.toCharArray();
+
+        for (int i = PalindromArray.length - 1; i > 0; i--) {
+
+
+            if(PalindromArray[i] == PalindromArray[i2])
+            {
+                i2++;
+
+            }else{
+                iHilfe = 1;
+            }
+            if(iHilfe == 1)
+            {
+                Ausgabe = false;
+            }
+
+
+
+        }
+
+
+        return Ausgabe;
+    }
+
+
 
 }
