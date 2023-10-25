83b38f98cf1b3505a4d0afe587e2193e458ed250
lukas <lukas.schuler@student.htldornbirn.at>
2023-10-25 09:30:06 +0200

    Pa

---
+++ src/at/lus/basic/Car.java
@@ -0,0 +1,57 @@
+package at.lus.basic;
+
+public class Car {
+
+    public double verbrauch;
+    public double fuel;
+    public String marke;
+    public String serie;
+    public String farbe;
+
+
+
+    public void drive (){
+        this.fuel = this.fuel - verbrauch;
+        System.out.println("Am fahre");
+    }
+
+    public void brea() {
+
+        System.out.println("I brems");
+    }
+
+    public void turboboost(){
+
+        double vergleich = 0;
+
+        vergleich = this.fuel/100;
+        if (vergleich < 0.1){
+            System.out.println("Not enough fuel");
+        }
+        else{
+            System.out.println("Turbo Boost activated");
+        }
+
+    }
+
+    public void honk(int amountOfRepetitions){
+
+        for (int i = 0; i < amountOfRepetitions; i++) {
+
+            System.out.println("Tuuuuuuuuut");
+        }
+
+
+
+    }
+
+    public void getRemainingRange(){
+
+        double rest = 0;
+
+        rest = this.fuel/ verbrauch;
+
+        System.out.println("Sie können noch " + rest + " mal fahren");
+    }
+}
+
---
+++ src/at/lus/basic/Das_erste_Objekt.java
@@ -0,0 +1,38 @@
+package at.lus.basic;
+import at.lus.basic.Car;
+
+
+public class Das_erste_Objekt {
+
+    public static void main(String[] args){
+
+        Car c1 = new Car();
+        c1.marke = "Audi";
+        c1.verbrauch = 7;
+        c1.serie = "A1234";
+        c1.farbe = "Schwarz";
+        c1.fuel = 50;
+
+        Car c2 = new Car();
+        c2.marke = "Mercedes";
+        c2.verbrauch = 5;
+        c2.serie = "B1234";
+        c2.farbe = "Grau";
+        c2.fuel = 60;
+
+
+        System.out.println(c2.fuel);
+        c2.drive();
+        c2.honk(5);
+        c2.turboboost();
+        c2.brea();
+        c2.drive();
+        c2.getRemainingRange();
+        c2.brea();
+
+
+
+
+    }
+
+}
