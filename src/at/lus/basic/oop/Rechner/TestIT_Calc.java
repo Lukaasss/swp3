2427b36d93605712a9e3525f7b0e6c1350543698
lukas <lukas.schuler@student.htldornbirn.at>
2024-01-05 20:55:33 +0100

    Kamera fertig

--- src/at/lus/basic/oop/Rechner/TestIT_Calc.java
+++ src/at/lus/basic/oop/Rechner/TestIT_Calc.java
@@ -1,15 +1,14 @@
 package at.lus.basic.oop.Rechner;
 
-public class TestIT_Calc{
+public class TestIT_Calc {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
 
-
         System.out.println("Addieren: " + calculator.add(5, 3));
-        System.out.println("Subtrahieren: " + calculator.sub(5,3));
-        System.out.println("Multiplizieren: " + calculator.mult(5,3));
-        System.out.println("Dividieren: " +calculator.div(5,2));
+        System.out.println("Subtrahieren: " + calculator.sub(5, 3));
+        System.out.println("Multiplizieren: " + calculator.mult(5, 3));
+        System.out.println("Dividieren: " + calculator.div(5, 2));
 
         System.out.println("Sinus: " + Sciencecalc.sin(2));
         System.out.println("Cosinus: " + Sciencecalc.cos(3));
